8b55796b4e1def9cd3bffada6149b59c6def94f3
shukl <shukl@DESKTOP-8BCVFBS>
2024-01-18 13:37:28 +0530

    Commit

---
+++ IntroToJava/src/com/edubridgecompile/CompileInputDemo.java
@@ -0,0 +1,29 @@
+package com.edubridgecompile;
+
+public class CompileInputDemo {
+
+	public static void main(String[] args) {
+		byte num1=127;
+		short num2 = 3433;
+		int num3=34567116;
+		long num4=9834567890L;
+		char c='X';
+		String str="EduBridge";
+		float num5=53.6f;
+		double num6=54.5d;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(num5);
+		System.out.println(num6);
+		System.out.println(str);
+		System.out.println(c);
+		
+		
+		
+		
+		
+	}
+
+}
